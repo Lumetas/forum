e97fab06bc385a44ce35db9238fec416349823ee
Lumetas <98038223+Lumetas@users.noreply.github.com>
2024-08-06 20:05:32 +0500

    Add files via upload

--- api/search.php
+++ api/search.php
@@ -1,10 +1,10 @@
 <?php
-include ('..\config.php');
-include ('..\lib\functions.php');
-include ('..\db\user_mysql_db.php');
-include ('..\db\search_data.php');
-include ('..\db\topics_mysql_db.php');
-include ('..\db\mysql_db.php');
+include ('../config.php');
+include ('../lib/functions.php');
+include ('../db/user_mysql_db.php');
+include ('../db/search_data.php');
+include ('../db/topics_mysql_db.php');
+include ('../db/mysql_db.php');
 
 $db = new mysql_db_functions($db_host, $db_user, $db_password, $db_name);
 
