af38cca061a6a01730690dd709f89c61fd7002ac
Lumetas <98038223+Lumetas@users.noreply.github.com>
2024-08-06 20:09:09 +0500

    Update createtheme.php

--- api/createtheme.php
+++ api/createtheme.php
@@ -1,10 +1,10 @@
 <?php
-include ('..\config.php');
-include ('..\lib\functions.php');
-include ('..\db\user_mysql_db.php');
-include ('..\db\search_data.php');
-include ('..\db\topics_mysql_db.php');
-include ('..\db\mysql_db.php');
+include ('../config.php');
+include ('../lib/functions.php');
+include ('../db/user_mysql_db.php');
+include ('../db/search_data.php');
+include ('../db/topics_mysql_db.php');
+include ('../db/mysql_db.php');
 
 $db = new mysql_db_functions($db_host, $db_user, $db_password, $db_name);
 // echo "<pre>";
@@ -12,7 +12,7 @@ $db = new mysql_db_functions($db_host, $db_user, $db_password, $db_name);
 // echo "</pre>";
 if ($db->search_user_data_with_session($_COOKIE['username'], $_COOKIE['session']) == NULL){
     $error = "You can't do this :(";
-    include('..\pages\404.php');
+    include('../pages/404.php');
     exit();
 }
 $themename = $_POST['name'];
